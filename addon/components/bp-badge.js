b8f89951c07ef5cc29fdea471b6a4394bb85bf4e
Frank <wsgoodman@163.com>
2019-02-21 10:54:14 +0800

    bp-badge 完成
    完全利用 bs 的样式。

---
+++ addon/components/bp-badge.js
@@ -0,0 +1,31 @@
+import Component from '@ember/component';
+import layout from '../templates/components/bp-badge';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+	layout,
+	tagName: 'span',
+	classNames: ['badge'],
+	classNameBindings: ['currentType', 'pill:badge-pill'],
+	/**
+	 * Property to show type of badge
+	 * @property type
+	 * @type string
+	 * @default 'primary'
+	 * @public
+	 */
+	type: 'primary',
+	currentType: computed('type', function () {
+		let type = this.get('type');
+
+		return `badge-${type}`;
+	}),
+	/**
+	 * Property to pill badges
+	 * @property pill
+	 * @type boolean
+	 * @default false
+	 * @public
+	 */
+	pill: false
+});
