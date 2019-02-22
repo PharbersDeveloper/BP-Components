2021e624f9fb59514417a3088f279614e0fd991a
Frank <wsgoodman@163.com>
2019-02-22 19:03:55 +0800

    重写 choose 的样式；提取共有属性。

--- addon/components/choose-group.js
+++ addon/components/choose-group.js
@@ -5,6 +5,9 @@ import { isArray, A } from '@ember/array';
 
 export default Component.extend({
 	layout,
+	tagName: 'section',
+	classNames: ['choose-group'],
+	classNameBindings: ['vertical'],
 	/**
 	 * @property chooseItemComponent
 	 * @type {String}
