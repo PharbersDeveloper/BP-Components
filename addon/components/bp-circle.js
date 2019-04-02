9bd41dbacbeb9885d5f4d6ed248207f6b25101ad
Frank <wsgoodman@163.com>
2019-04-02 13:40:46 +0800

    为 bp-circle 添加千分符

--- addon/components/bp-circle.js
+++ addon/components/bp-circle.js
@@ -51,17 +51,44 @@ export default Component.extend({
 	 * @public
 	 */
 	circleColor: A(['#00875A', '#F4F5F7']),
+	// 千分位，应该提取出去
+	formatNum(number) {
+		if (number.length <= 3) {
+			return number;
+		}
+		if (!/^(\+|-)?(\d+)(\.\d+)?$/.test(number)) {
+			return number;
+		}
+		let a = RegExp.$1,
+			b = RegExp.$2,
+			c = RegExp.$3,
+			re = new RegExp();
 
+		re.compile('(\\d)(\\d{3})(,|$)');
+		while (re.test(b)) {
+			b = b.replace(re, '$1,$2$3');
+		}
+		return String(String(a) + b) + c;
+	},
 	generateOption() {
 		let { seriesName, circleData, circleColor } =
-			this.getProperties('seriesName', 'circleData', 'circleColor');
+			this.getProperties('seriesName', 'circleData', 'circleColor'),
+			that = this;
 
 		return {
 			tooltip: {
 				trigger: 'item',
-				formatter: '{a} <br/>{b}: {c} ({d}%)'
+				formatter: function (params) {
+					let sName = params.seriesName,
+						name = params.name,
+						value = that.formatNum(params.value),
+						percent = params.percent;
+
+					return `${sName}<br/>${name}:${value}(${percent}%)`;
+				},
+				alwaysShowContent: false,
+				hideDelay: '60'
 			},
-			// color: ['#00875A', '#36B37E ', '#57D9A3 ', '#79F2C0'],
 			color: circleColor,
 			legend: {
 				show: false
