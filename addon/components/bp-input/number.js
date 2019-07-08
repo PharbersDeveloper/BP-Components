e461cdc51690ec7e2b6befe313beac5dfed86958
frank <wangsen9108@gmail.com>
2019-07-08 18:50:53 +0800

    打印的修改。

--- addon/components/bp-input/number.js
+++ addon/components/bp-input/number.js
@@ -15,17 +15,11 @@ export default BpInput.extend({
 				value = event.target.value,
 				verify = isEmpty(event.data) ? true : verifyNumber.test(event.data);
 
-
-			console.log(previousValue);
-			console.log(event);
-			console.log(verify);
-			console.log(event.target.value);
 			this.set('value', verify ? value : previousValue);
 			this.set('verify', !verify);
 			if (!verify) {
 				this.get('onWarning')();
 			}
-			console.log(this.get('value'));
 			event.target.value = this.get('value');
 			if (maxLength !== 0) {
 				this.get('onChange')(value.slice(0, maxLength));
