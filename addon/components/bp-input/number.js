e986482c98ea99651c927b49d838ca69bdbab94a
frank <wangsen9108@gmail.com>
2019-07-08 18:46:41 +0800

    验证的修改。

--- addon/components/bp-input/number.js
+++ addon/components/bp-input/number.js
@@ -1,22 +1,31 @@
 import BpInput from 'bp-components/components/bp-input';
 import layout from '../../templates/components/bp-input';
+import { isEmpty } from '@ember/utils';
 
 export default BpInput.extend({
 	layout,
-	onWarning() { },
+	onWarning() {
+
+	},
 	actions: {
 		change(event) {
 			let verifyNumber = /^\d+$/,
 				previousValue = this.get('value'),
 				maxLength = this.get('maxLength'),
 				value = event.target.value,
-				verify = verifyNumber.test(event.data);
+				verify = isEmpty(event.data) ? true : verifyNumber.test(event.data);
 
 
+			console.log(previousValue);
+			console.log(event);
+			console.log(verify);
+			console.log(event.target.value);
 			this.set('value', verify ? value : previousValue);
+			this.set('verify', !verify);
 			if (!verify) {
 				this.get('onWarning')();
 			}
+			console.log(this.get('value'));
 			event.target.value = this.get('value');
 			if (maxLength !== 0) {
 				this.get('onChange')(value.slice(0, maxLength));
