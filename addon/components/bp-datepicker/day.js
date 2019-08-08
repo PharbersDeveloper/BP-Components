7ff20673e8ac02d3d21e14a3b1d50189f56b942f
frank <wangsen9108@gmail.com>
2019-08-08 19:57:18 +0800

    着手 bp-datepicker 的开发

---
+++ addon/components/bp-datepicker/day.js
@@ -0,0 +1,57 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/bp-datepicker/day';
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+
+export default Component.extend({
+	layout,
+	tagName: 'td',
+	classNameBindings: ['isToday:today', 'active'],
+	isActive() {
+		let choosedDate = this.get('choosedDate'),
+			currentDate = this.get('date');
+
+		if (choosedDate.date === currentDate.showDate && choosedDate.year === currentDate.year && choosedDate.month === currentDate.month) {
+			this.set('active', true);
+		} else {
+			this.set('active', false);
+		}
+	},
+	isToday: alias('today.isToday'),
+	showText: alias('today.showText'),
+	today: computed('date', function () {
+		let date = this.get('date'),
+			now = new Date(),
+			year = now.getFullYear(),
+			month = now.getMonth() + 1,
+			day = now.getDate();
+
+		if (date.year === year && date.month === month && date.showDate === day) {
+			return {
+				isToday: true,
+				showText: '今'
+			};
+		}
+		return {
+			isToday: false,
+			showText: date.showDate
+		};
+	}),
+	onClick() { },
+	didReceiveAttrs() {
+		this._super(...arguments);
+		this.isActive();
+	},
+	didUpdateAttrs() {
+		this._super(...arguments);
+		this.isActive();
+	},
+	click() {
+		let date = this.get('date');
+
+		this.set('active', true);
+		this.get('onClick')(date);
+	}
+}).reopenClass({
+	positionalParams: ['date']
+});
